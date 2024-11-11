abf8b14d6c9cf926efdd2bb6b9eb1c50d558afbf
Yasin Tepeli <ytepeli@login1.hpc.tudelft.nl>
2024-11-11 14:42:53 +0100

    Removing unnecessary files

--- prepare-cluster.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-cp *.py /w/staff-umbrella/JGMasters/2122-mathijs-de-wolf/code/
-cp experiment-analysis/*.py /w/staff-umbrella/JGMasters/2122-mathijs-de-wolf/code/experiment-analysis/
